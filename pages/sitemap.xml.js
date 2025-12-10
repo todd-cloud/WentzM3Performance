cac4f4b685a4a1834208f40bdf488a5c404cc571
todd-cloud <todd@wentzdesigns.com>
2025-12-10 14:18:01 -0600

    Add server-side generation for sitemap.xml

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,37 @@
+export const getServerSideProps = async ({ res }) => {
+  const baseUrl = "https://wentzm3performance.vercel.app";
+
+  // Define your static routes
+  const staticPages = [
+    "",
+    "api/products",
+    "api/gallery",
+    "api/meets"
+  ];
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${staticPages
+    .map(
+      (page) => `
+    <url>
+      <loc>${baseUrl}/${page}</loc>
+      <lastmod>${new Date().toISOString()}</lastmod>
+      <changefreq>weekly</changefreq>
+      <priority>${page === "" ? "1.0" : "0.8"}</priority>
+    </url>
+  `
+    )
+    .join("")}
+</urlset>`;
+
+  res.setHeader("Content-Type", "text/xml");
+  res.write(sitemap);
+  res.end();
+
+  return { props: {} };
+};
+
+export default function Sitemap() {
+  return null;
+}
